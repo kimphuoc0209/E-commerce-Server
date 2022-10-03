8d1841146049a35e53744c53c0cf4bd018ab30c6
lethanhtrung7412 <lethanhtrung7412@gmail.com>
2022-10-03 16:14:12 +0700

    Setup server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("This is test");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, console.log(`Server running on http://localhost:5000`));
