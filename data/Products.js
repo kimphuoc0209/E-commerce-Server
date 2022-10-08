7f33d09d967baa2671ff2c0bfbb84e17321990d1
lethanhtrung7412 <lethanhtrung7412@gmail.com>
2022-10-08 10:58:05 +0700

    Create Models, Routes, insert data

---
+++ data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "Vảy Ốc Đỏ",
+    image: "/images/1.png",
+    description:
+      "Cây thủy sinh Vảy ốc đỏ là dòng cây dễ trồng thường được anh em chơi thủy sinh sử dụng làm trang trí bể cá, bể thủy sinh. Do đặc thù không cần CO2 vẫn phát triển tốt nên Vảy ốc đỏ được rất nhiều anh em chơi thủy sinh yêu thích.",
+    price: 25.0,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Ráy Nana Japan",
+    image: "/images/2.png",
+    description:
+      "Ráy nana japan là một trong những dòng cây thủy sinh đẹp, dễ trồng, dễ chăm sóc đặc biệt là ráy nana Japan có thể trồng được nhiều vị trí bên trong hồ thủy sinh.",
+    price: 50.0,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Cây dương xỉ Mỹ Nhân",
+    image: "/images/3.png",
+    description:
+      "Dương xỉ Mỹ Nhân là dòng cây thủy sinh đẹp, dễ trồng có thể không cần đất nền và co2 vẫn có thể phát triển tốt. Vị trí thường trồng gồm trung cảnh, hậu cảnh hoặc điểm bố cục.",
+    price: 60.0,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Cây lan nước",
+    image: "/images/4.png",
+    description:
+      "Lan nước là một trong những dòng cây thủy sinh rất dễ trồng, bạn có thể bỏ chúng ở mọi nơi chỉ cần có điều kiện ánh sáng và ngập nước là chúng có thể phát triển.",
+    price: 25.0,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Cá Chuột Pygmy",
+    image: "/images/5.png",
+    description:
+      "Chuột Pygmy thuộc dòng cá chuột có thể nói là nhỏ nhất trong các dòng cá chuột với đặc điểm nổi bật là bơi theo đàn và có kích thước nhỏ nên Chuột Pygmy rất được yêu thích.",
+    price: 30.0,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Tép Yamato",
+    image: "/images/6.png",
+    description:
+      "Tép Yamato là một dòng tép cảnh nước lợ, chúng được anh em chơi thủy sinh rất yêu thích bởi công năng của loài tép này là xử lý các rêu hại bên trong hồ thủy sinh.",
+    price: 40.0,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./Routes/ProductRoutes.js";
 
+dotenv.config();
+connectDatabase();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, console.log(`Server running on http://localhost:5000`));
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
